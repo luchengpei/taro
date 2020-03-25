4704aea3cb7a5daa46747d11749d93fddb04b595
luchengpei <3307602305@qq.com>
2020-03-25 21:23:27 +0800

    aaae

--- src/pages/cp/cp.js
+++ src/pages/cp/cp.js
@@ -7,7 +7,7 @@ export default class Cp extends Component{
             this.state={
                 index:0,
                 list:[
-                    '循环1','循环2','循环3'
+                    '循环1','循环2','循环33'
                 ]
             }
         }
